fa21f0c80bab250520cd9c981ed362b62db33df6
iweinacker <>
2023-08-11 09:56:16 +0000

    Testing models

---
+++ todo/test_models.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from .models import Item
+
+
+class TestModels(TestCase):
+
+    def test_done_default_to_False(self):
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertFalse(item.done)
+        
